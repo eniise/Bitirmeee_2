e48575ffeaf45b6d558463eeb498065e75a24f7d
Elif Acar <elifac.r1997@gmail.com>
2021-12-10 03:37:29 +0300

    commit test

--- app/src/main/java/com/enise/bitirme_2/MainActivity.java
+++ app/src/main/java/com/enise/bitirme_2/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this.getBaseContext(),"",Toast.LENGTH_LONG);
+        Toast.makeText(this.getBaseContext(),"",Toast.LENGTH_LONG).show();
     }
 }
