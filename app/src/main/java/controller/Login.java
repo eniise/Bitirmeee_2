5ad9cdb6e7687fde54315710403ff10ae97e7a85
Mustafa GÜRSES <gursesmm@gmail.com>
2021-12-12 20:24:53 +0300

    New design

--- app/src/main/java/controller/Login.java
+++ app/src/main/java/controller/Login.java
@@ -3,6 +3,7 @@ package controller;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -11,13 +12,19 @@ import com.enise.bitirme_2.R;
 
 public class Login extends Activity {
     TextView registerPage;
+    Button btnLogin;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_page);
         registerPage = findViewById(R.id.registerPage);
+        btnLogin = findViewById(R.id.btnLogin);
         registerPage.setOnClickListener(v -> {
             Intent intent = new Intent(v.getContext(), Register.class);
+            startActivity(intent);
+        });
+        btnLogin.setOnClickListener(v -> {
+            Intent intent = new Intent(v.getContext(), Main.class);
             startActivity(intent);
         });
     }
--- app/src/main/java/controller/Main.java
+++ app/src/main/java/controller/Main.java
@@ -5,9 +5,12 @@ import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 
+import com.enise.bitirme_2.R;
+
 public class Main extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_page);
     }
 }
