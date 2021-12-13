56b5c8a73457ec80dc402ec5692fd89a4f9f3f80
Mustafa GÜRSES <gursesmm@gmail.com>
2021-12-13 19:18:34 +0300

    Tasarim devam

--- app/src/main/java/controller/Main.java
+++ app/src/main/java/controller/Main.java
@@ -1,16 +1,61 @@
 package controller;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+import android.widget.Toolbar;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 
 import com.enise.bitirme_2.R;
 
 public class Main extends Activity {
+    Toolbar mToolBar;
+    Context context;
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_page);
+        mToolBar = findViewById(R.id.toolbar);
+        SetApplicationToolbar(mToolBar);
+        context = this;
+    }
+
+    @SuppressLint("RestrictedApi")
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.ust_menu, menu);
+        return true;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        Toast.makeText(context,"asdjasjdasd",Toast.LENGTH_LONG).show();
+        switch (item.getItemId()){
+            case R.id.DetailFiltering:
+                Toast.makeText(context,"Detaylı arama sayfası detail",Toast.LENGTH_LONG).show();
+                break;
+            case R.id.MapFiltering:
+                Toast.makeText(context,"Detaylı arama sayfası map",Toast.LENGTH_LONG).show();
+                break;
+        }
+        return true;
+    }
+    private void SetApplicationToolbar(Toolbar toolbar){
+        setActionBar(mToolBar);
+        mToolBar.inflateMenu(R.menu.ust_menu);
+        getActionBar().setTitle("");
     }
 }
