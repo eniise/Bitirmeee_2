895213ebbc0fab3db9cf6310e3e70e3a23a200ad
Mustafa GÜRSES <gursesmm@gmail.com>
2021-12-15 00:20:02 +0300

    Tasarim devam

--- app/src/main/java/controller/Maps.java
+++ app/src/main/java/controller/Maps.java
@@ -1,6 +1,8 @@
 package controller;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.location.Address;
 import android.location.Geocoder;
 import android.os.Bundle;
@@ -54,7 +56,21 @@ public class Maps extends AppCompatActivity implements OnMapReadyCallback {
                 List<Address> addresses = gcd.getFromLocation(latLng.latitude, latLng.longitude, 1);
                 try {
                     String adress = addresses.get(0).getAddressLine(0);
-                    String find_ilce = Pattern.compile("[\\,\"]").split(adress)[2].split(" ")[2].split("/")[0];
+                    String find_ilce = Pattern.compile("[\\,\"]")
+                            .split(adress)[2]
+                            .split(" ")[2]
+                            .split("/")[0];
+                    new AlertDialog.Builder(context)
+                            .setTitle("İlçe bulundu")
+                            .setMessage(find_ilce+" isimli ilçeye tıkladınız, bu ilçedeki öğretmenler anasayfada listelensin mi?")
+                            .setPositiveButton(R.string.yonlendir, (dialog, which) -> {
+                                Toast.makeText(context,"API geliştireyim seni yönlendiririm.",Toast.LENGTH_LONG).show();
+                            })
+                            .setNegativeButton(R.string.hayir, ((dialog, which) -> {
+                                Toast.makeText(context,"Madem öyle defol başka bir ilçe bul.",Toast.LENGTH_LONG).show();
+                            }))
+                            .setIcon(android.R.drawable.ic_dialog_info)
+                            .show();
                 }
                 catch (IndexOutOfBoundsException e){
                     Toast.makeText(context,"Tıkladığın yerden ilçe bilgisi alamadım. Başka bir yere tıkla.",Toast.LENGTH_LONG).show();
